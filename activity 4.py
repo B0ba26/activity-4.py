587a89df8c92f31de5782b4d4d59128ca2af2570
B0ba26 <boba135t24@gmail.com>
2024-12-22 10:46:20 -0800

    Add files via upload

---
+++ activity 4.py
@@ -0,0 +1,37 @@
+##There are five trees in Jack's front yard. He checks each tree to find out how tall it is in inches and writes the height on a sheet of paper. Jack's list: 98, 94, 41, 96, and 11. What is the average height of a tree in Jack's front yard?
+
+tree1=98
+tree2=94
+tree3=41
+tree4=96
+tree5=11
+
+sum=tree1+tree2+tree3+tree4+tree5
+print(sum)
+avg=sum/5
+print(avg)
+##Write a program to calculate the number of notes in the given amount?
+#5650
+#56x100
+#50x1
+#10x0
+
+amount=int(input("Enter the amount: "))
+
+note1=amount//100
+note2=(amount%100)//50
+note3=((amount%100)%50)//10
+print("notes of 100's:",note1)
+print("note of 50:;'s",note2)
+print("notes of 10:'s",note3)
+##Raj scored 40, 70, 50 and 60 out of 100 in maths, science, Hindi and English. Find the percentage he got?
+score1=40
+score2=70
+score3=50
+score4=60
+sum=score1+score2+score3+score4
+print(sum)
+avg=sum/400
+print(avg)
+percentage=avg*100
+print(percentage)
